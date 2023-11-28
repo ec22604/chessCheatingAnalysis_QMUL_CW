9b32dc35e8e481918ef071d9270157e958518362
ec22604 <ec22604@qmul.ac.uk>
2023-11-28 14:23:45 +0000

    Collected all of Hikaru's games

--- collection.py
+++ collection.py
@@ -6,16 +6,18 @@ HEADERS = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/5
 s = requests.Session()
 r = s.get(GAME_ARCHIVES_URL,headers=HEADERS)
 archives = json.loads(r.content.decode())["archives"]
-for archive in archives:
+for i,archive in enumerate(archives):
     games = json.loads(s.get(archive,headers=HEADERS).content.decode())["games"]
     with open("games.csv","a") as f:
         for game in games:
-            hColour = "Black"
-            oColour = "White"
+            hColour = "black"
+            oColour = "white"
             if game["white"]["username"] == "Hikaru":
-                hColour = "White"
-                oColour = "Black"
-            
-            f.writelines(','.join([archive[-1:-8].replace("/","-"),hColour,game[hColour]["result"],game]))
-
-    exit()
+                hColour = "white"
+                oColour = "black"
+            try:
+                f.writelines(','.join([str(archive)[::-1][0:7][::-1].replace("/","-"),hColour,game[hColour]["result"],game[oColour]["result"],str(game["time_control"]),repr(game["pgn"]),game["rules"],str(game[hColour]["rating"]),str(game[oColour]["rating"]),str(game["end_time"])])+"\n")
+            except Exception as e:
+                print(e)
+                print(game)
+    print("%f%% archives completed (%d/%d)" %((i+1)/len(archives)*100,i+1,len(archives)))
