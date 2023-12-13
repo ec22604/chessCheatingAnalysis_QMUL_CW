96e8800c2286f21ca446b52d2da278b1659ec05f
ec22604 <ec22604@qmul.ac.uk>
2023-12-13 16:40:06 +0000

    updated analysis

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@ def extractTimesFromPGN(row):
     for i in range(len(row)):
         #if this is true, we know that we are about to find a time to add
         if row[i-3:i] == "clk":
-            #if it's the very last time in the PGN, the syntax will be different
+            #if the second is a decimal, the time in the PGN is longer so adjust for that
             if row[i+9] == "}":
                 timeStr += row[i+1:i+8]+","
             else:
